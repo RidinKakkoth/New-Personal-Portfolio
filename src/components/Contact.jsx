7e079f8588eac879a0ff467bad76d1c3266c8dab
Ridin Kakkoth <k.ridin@ymail.com>
2023-11-25 12:08:57 +0530

    scroll align updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div name="contact" className="w-full h-screen bg-[#0a192f]  flex justify-center items-center p-4 ">
       <form method="POST" action="https://getform.io/f/e234d888-a566-4f15-b67f-898674ced754" className="flex flex-col max-w-[600px] w-full">
-        <div className="pb-5">
+        <div className="pb-3">
             <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300 " >Contact</p>
             <p className="text-gray-300 py-4">Submit the form below or shoot me an email - <span className="font-bold">k.ridin@ymail.com</span></p>
         </div>
